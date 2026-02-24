f728dc93e524b6efb6f51a0695b22ac565d6f27e
rastar12 <eugenechanzu@gmail.com>
2026-02-24 17:57:06 +0300

    how to use path paramaters in python

--- myApi.py
+++ myApi.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 
 app= FastAPI()
+
+students={
+    1:{"name":"John","age":20 ,"course":"Computer Science"}
+}
 
 @app.get("/")
 def index():
     return{"message":"Hello world"}
+
+# path parameter
+@app.get("/students/{student_id}")
+def get_student(student_id:int=Path(description="The ID of the student you want to view",gt=0,lt=3)):
+    return students[student_id] 
 
